bd02e3f87772ed1c1232187623451db7cd09cbe9
SOOKYOJ <sookyo0810@naver.com>
2024-02-20 21:30:03 +0900

    third commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/app">Pokédex</Navbar.Brand>
+        <Navbar.Brand href="/">Pokédex</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
